b80441a33e41319dbccbe9da9ea789eab91eb44f
Youjeong Park <youjeongp@logicsquare.com>
2025-02-24 19:57:57 -0800

    feat : persona

---
+++ src/main/java/com/bside/potenday/dto/Message.java
@@ -0,0 +1,86 @@
+package com.bside.potenday.dto;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+
+@Data
+@Builder
+public class Message {
+    private static String designPersonaPrompt;
+    private static String reactionPrompt;
+    private static String summaryPrompt;
+    private static String keywordPrompt;
+    private ROLE role;
+    private String content;
+
+    public enum ROLE {
+        system, user, assistant
+    }
+
+    public static Message creatUserOf(String content) {
+        return Message.builder()
+                .role(ROLE.user)
+                .content(content)
+                .build();
+    }
+
+    public static Message creatSystemOf(String content) {
+        return Message.builder()
+                .role(ROLE.system)
+                .content(content)
+                .build();
+    }
+
+    public static Message createDesignPersonaSystemOf() {
+        return Message.builder()
+                .role(ROLE.system)
+                .content(new String(Base64.getDecoder().decode(designPersonaPrompt)))
+                .build();
+    }
+
+    public static Message createReactionOf() {
+        return Message.builder()
+                .role(ROLE.system)
+                .content(new String(Base64.getDecoder().decode(reactionPrompt)))
+                .build();
+    }
+
+    public static Message createSummaryOf() {
+        return Message.builder()
+                .role(ROLE.system)
+                .content(new String(Base64.getDecoder().decode(summaryPrompt)))
+                .build();
+    }
+
+    public static Message createKeywordOf() {
+        return Message.builder()
+                .role(ROLE.system)
+                .content(new String(Base64.getDecoder().decode(keywordPrompt)))
+                .build();
+    }
+
+    @Component
+    public static class Config {
+        @Value("${clova.prompt.design}")
+        private String design;
+        @Value("${clova.prompt.discover.reaction}")
+        private String reaction;
+        @Value("${clova.prompt.discover.summary}")
+        private String summary;
+        @Value("${clova.prompt.discover.keyword}")
+        private String keyword;
+
+        @PostConstruct
+        public void init() {
+            designPersonaPrompt = design;
+            reactionPrompt = reaction;
+            summaryPrompt = summary;
+            keywordPrompt = keyword;
+        }
+    }
+}
---
+++ src/main/java/com/bside/potenday/dto/Result.java
@@ -0,0 +1,17 @@
+package com.bside.potenday.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Result {
+    private Message message;
+}
