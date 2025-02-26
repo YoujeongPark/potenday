165736892c281f08f58da8dbc03fa172ce300e35
lrh1013 <dolefulmoon@gmail.com>
2025-02-27 00:57:47 +0900

    add : image

--- src/frontend/src/assets/images.js
+++ src/frontend/src/assets/images.js
@@ -1,9 +1,10 @@
 // 이미지 export
 export { default as logo } from './common/logo.svg';
-export { default as character } from './common/character.svg';
+export { default as character } from './common/character.png';
 export { default as book } from './icon/book.svg';
 export { default as game } from './icon/game.svg';
 export { default as home } from './icon/home.svg';
+export { default as arrow } from './icon/arrow.svg';
 export { default as category01 } from './icon/category01.svg';
 export { default as category02 } from './icon/category02.svg';
 export { default as category03 } from './icon/category03.svg';
