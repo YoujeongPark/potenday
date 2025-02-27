d24c9c701c27a7fddbb9adf0a81c7ed9a0ec7099
Youjeong Park <youjeongp@logicsquare.com>
2025-02-27 10:41:09 -0800

    feat : frontend랑 category 합치기

--- src/main/java/com/bside/potenday/model/Category.java
+++ src/main/java/com/bside/potenday/model/Category.java
@@ -12,7 +12,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Category {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String categoryName;
